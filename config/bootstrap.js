eaac7803b3e185027b21484b6355ac4eec6640a7
LaneLu <1537167855@qq.com>
2018-11-08 20:18:45 +0800

    login AJAX

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,10 +9,31 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = async function (cb) {   
+module.exports.bootstrap = async function (cb) {
+  sails.bcrypt = require('bcrypt');
+  const saltRounds = 10;
+
+
+  const hash = await sails.bcrypt.hash('123456', saltRounds);
+
   await User.createEach([
-    { "username": "admin", "password": "123456" },
-    { "username": "boss", "password": "123456" }
+    { "username": "admin", "password": hash },
+    { "username": "boss", "password": hash }
+    // etc.
+  ]);
+  await Event.createEach([
+    {
+      "eventname": "A simple event",
+      "shortdescription": "This is an innitial event",
+      "fulldescription": "This event is saved in bootstrap.js",
+      "imageurl": "https://pic1.zhimg.com/80/v2-40b17763b4eb99ed91c3be971516cc98_hd.jpg",
+      "organizer": "o1",
+      "eventdate": "11/26/2018",
+      "time": "3:00",
+      "venue": "v1",
+      "quota": "123",
+      "highlight": "Highlighted Event",
+    },
     // etc.
   ]);
   // It's very important to trigger this callback method when you are finished
