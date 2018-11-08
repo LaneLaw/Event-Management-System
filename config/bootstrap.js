9d0da2443538e6bd37af87f1ac15e604bf419d33
LaneLu <1537167855@qq.com>
2018-11-08 11:04:36 +0800

    log in function is complete

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,8 +9,12 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function(cb) {
-
+module.exports.bootstrap = async function (cb) {   
+  await User.createEach([
+    { "username": "admin", "password": "123456" },
+    { "username": "boss", "password": "123456" }
+    // etc.
+  ]);
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
